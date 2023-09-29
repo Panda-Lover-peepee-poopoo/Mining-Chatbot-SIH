334fd239437719b7edb1d3820848d6d2a53607fa
PandaLover <68319739+Panda-Lover-peepee-poopoo@users.noreply.github.com>
2023-09-29 22:17:20 +0530

    Updated the prototype

--- test.py
+++ test.py
@@ -6,22 +6,20 @@ from nltk.corpus import stopwords
 nlp = spacy.load("en_core_web_sm")
 
 """
-function name: connect_to_database
-
-function description: Establishes a connection to the MySQL database.
-
-Parameters:
-- host (str): Database host address.
-- user (str): Username for database authentication.
-- password (str): Password for database authentication.
-- database (str): Name of the database to connect to.
-
-Return Value:
-- conn (MySQLConnection): MySQL database connection object.
-- cursor (MySQLCursor): Cursor object to execute SQL queries.
+    function name: connect_to_database
+    function description: Establishes a connection to the MySQL database.
+    Parameters:
+    - host (str): Database host address.
+    - user (str): Username for database authentication.
+    - password (str): Password for database authentication.
+    - database (str): Name of the database to connect to.
+    Return Value:
+    - conn (MySQLConnection): MySQL database connection object.
+    - cursor (MySQLCursor): Cursor object to execute SQL queries.
 """
 
 def connect_to_database(host, user, password, database):
+
     conn = mysql.connector.connect(
         host=host,
         user=user,
@@ -34,50 +32,49 @@ def connect_to_database(host, user, password, database):
     return conn, cursor
 
 """
-function name: extract_keywords
-
-function description: Extracts keywords from the user query after lemmatization and removing stopwords.
-
-Parameters:
-- query (str): User input query.
-
-Return Value:
-- keywords (list): List of lemmatized, non-stopword keywords extracted from the query.
+    function name: extract_keywords
+    function description: Extracts keywords from the user query after lemmatization and removing stopwords.
+    Parameters:
+    - query (str): User input query.
+    Return Value:
+    - keywords (list): List of lemmatized, non-stopword keywords extracted from the query.
 """
 
 def extract_keywords(query):
+
     doc = nlp(query)  # Use spaCy for lemmatization
     stop_words = set(stopwords.words('english'))
     keywords = [token.lemma_.lower() for token in doc if token.is_alpha and token.lemma_.lower() not in stop_words]
     return keywords
 
 """
-function name: search_data
-
-function description: Searches for relevant information in the MySQL database based on extracted entities from the user query.
-
-Parameters:
-- user_query (str): User input query.
-- cursor (MySQLCursor): The cursor object to execute SQL queries.
-- max_results (int, optional): Maximum number of results to return. Defaults to 5.
-- target_category (str, optional): Target category to filter results. Defaults to 'Development'.
-
-Return Value:
-- matched_results (list): List of tuples containing matched results (category, title, content).
+    function name: search_data
+    function description: Searches for relevant information in the MySQL database based on extracted entities from the user query.
+    Parameters:
+    - user_query (str): User input query.
+    - cursor (MySQLCursor): The cursor object to execute SQL queries.
+    - max_results (int, optional): Maximum number of results to return. Defaults to 5.
+    - target_category (str, optional): Target category to filter results. Defaults to None (no category filtering).
+    Return Value:
+    - matched_results (list): List of tuples containing matched results (category, title, content).
 """
 
-def search_data(user_query, cursor, max_results=5, target_category='Development'):
+def search_data(user_query, cursor, max_results=5, target_category=None):
+
     keywords = extract_keywords(user_query)
-    # print("Extracted Keywords:", keywords)
+    print("Extracted Keywords:", keywords)
     matched_results = []
 
     # Build a dynamic SQL query with multiple OR conditions for each keyword
-    query_str = f"SELECT category, title, content FROM mining_data WHERE {' OR '.join(['content COLLATE utf8mb4_general_ci LIKE %s'] * len(keywords))} AND category = %s"
+    query_str = f"SELECT category, title, content FROM mining_data WHERE {' OR '.join(['content COLLATE utf8mb4_general_ci LIKE %s'] * len(keywords))}"
     params = ['%' + keyword + '%' for keyword in keywords]
-    params.append(target_category)
 
-    # print("SQL Query:", query_str)
-    # print("Parameters:", params)
+    if target_category:
+        query_str += " AND category = %s"
+        params.append(target_category)
+
+    print("SQL Query:", query_str)
+    print("Parameters:", params)
 
     cursor.execute(query_str, params)
     results = cursor.fetchall()
@@ -89,15 +86,14 @@ def search_data(user_query, cursor, max_results=5, target_category='Development'
     return matched_results[:max_results]
 
 """
-function name: display_results
-
-function description: Displays the matched results in a formatted manner.
-
-Parameters:
-- results (list): List of tuples containing matched results (category, title, content).
+    function name: display_results
+    function description: Displays the matched results in a formatted manner.
+    Parameters:
+    - results (list): List of tuples containing matched results (category, title, content).
 """
 
 def display_results(results):
+
     print("-" * 60)
     for result in results:
         category, title, content = result
@@ -116,8 +112,7 @@ def main():
     # Establish a connection to the MySQL database
     conn, cursor = connect_to_database(db_host, db_user, db_password, db_database)
 
-    print(
-        "Welcome to the Mining Industry Chatbot. You can ask questions about mining Acts, Rules, Regulations, Guidelines, Methodology, or Corrigenda. Type 'exit' to quit.")
+    print("Welcome to the Mining Industry Chatbot. You can ask questions about mining Acts, Rules, Regulations, Guidelines, Methodology, or Corrigenda. Type 'exit' to quit.")
 
     while True:
         user_query = input("You: ").strip().lower()
@@ -134,14 +129,28 @@ def main():
             print("Chatbot: Goodbye! If you have more questions in the future, feel free to return.")
             break
 
-        # Search for user query in the database
-        matched_results = search_data(user_query, cursor)
+        # Determine the target category based on user input
+        target_category = None
+        if 'acts' in user_query:
+            target_category = 'Acts'
+        elif 'rules' in user_query:
+            target_category = 'Rules'
+        elif 'regulations' in user_query:
+            target_category = 'Regulations'
+        elif 'guidelines' in user_query:
+            target_category = 'Guidelines'
+        elif 'methodology' in user_query:
+            target_category = 'Methodology'
+        elif 'corrigenda' in user_query:
+            target_category = 'Corrigenda'
+
+        # Search for user query in the specified category
+        matched_results = search_data(user_query, cursor, target_category=target_category)
 
         if matched_results:
             display_results(matched_results)
         else:
-            print(
-                "Chatbot: I couldn't find any relevant information. Please try asking in a different way or specify a different topic.")
+            print("Chatbot: I couldn't find any relevant information. Please try asking in a different way or specify a different topic.")
 
 if __name__ == "__main__":
     main()
